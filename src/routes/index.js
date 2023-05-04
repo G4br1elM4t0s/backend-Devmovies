854c59c4418f7ef863e5f2895e076979e811a1ab
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-05-04 00:09:47 -0300

    feat: Concluido o controller de signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+const CreateUserController = require("../controllers/user/CreateUserController");
+
+//controller
+const createUserController = new CreateUserController();
+
 router.get("/", (req, res) => {
   res.send({ ok: true });
 });
+
+router.post("/signup", createUserController.handle);
 
 module.exports = router;
