f4c82f229046f335862d7b060fc125d746907da2
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-05-04 12:52:38 -0300

    feat: adição do controller de authentificacao

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 const CreateUserController = require("../controllers/user/CreateUserController");
+const AuthController = require("../controllers/user/AuthController");
 
 //controller
 const createUserController = new CreateUserController();
+const authController = new AuthController();
 
 router.get("/", (req, res) => {
   res.send({ ok: true });
 });
 
 router.post("/signup", createUserController.handle);
+router.post("/auth", authController.handle);
 
 module.exports = router;
