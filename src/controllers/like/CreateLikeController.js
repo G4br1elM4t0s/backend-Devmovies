20d213c8261487e9e76596fa78fb755eaf0527fe
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-05-04 12:53:13 -0300

    feat: adição da rota de like

---
+++ src/controllers/like/CreateLikeController.js
@@ -0,0 +1,18 @@
+const CreateLikeUseCase = require("../../models/like/useCase/createLike/CreateLikeUseCase");
+
+class CreateLikeController {
+  async handle(req, res) {
+    try {
+      const { userId } = req.params;
+      const { movieId, liked } = req.body;
+
+      const createLikeUseCase = new CreateLikeUseCase();
+
+      const result = await createLikeUseCase.execute(userId, movieId, liked);
+
+      res.status(201).json(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
---
+++ src/models/like/useCase/createLike/CreateLikeUseCase.js
@@ -0,0 +1,46 @@
+const prisma = require("../../../../db");
+const AppError = require("../../../../erros/AppErro");
+
+class CreateLikeUseCase {
+  async execute({ userId, movieId, liked }) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new AppError("User not found", 404);
+    }
+
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (!movie) {
+      throw new AppError("Movie not found", 404);
+    }
+
+    const like = await prisma.like.create({
+      data: {
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        movie: {
+          connect: {
+            id: movie.id,
+          },
+        },
+      },
+      liked,
+    });
+
+    return like;
+  }
+}
+
+module.exports = CreateLikeUseCase;
