f26477fca1dee35151991a228c06460d09788b0c
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-05-08 02:39:03 -0300

    feat: criado a tabela de streamer e a tabela de genrer

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const router = require("./routes");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(router);
 
